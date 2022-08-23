2496d6cc9387fb0bcc48cc7f82933a0f563229f2
KevinTheGreat <Westminsterkevindo@gmail.com>
2022-08-22 17:24:24 -0700

    Forgot that data type wasnt added

---
+++ Primitive Data Types/Data Types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// Tutorial 0: These are the most simplest data types you could have.
+	int integer = 0;
+
+	float floatNumber = 1.0;
+
+	char character = 'a';
+
+	// True or False
+	bool isBoolean = true; 
+
+	return 0;
+}
